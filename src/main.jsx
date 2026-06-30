1f74e1b050e1195f7116d8c9005e9c13e0aac456
Sarthak <sarthaksingh0116gmail.com>
2026-06-30 14:30:16 +0530

    Setup files

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/index.css";
 import "@fontsource/exo-2";
 import "@fontsource/tektur";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
